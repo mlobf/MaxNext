45a83a5ade5d2b96d8a21b3f1354def65187c73d
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-28 00:04:32 -0300

    test add with popOs

---
+++ reviewReactZero/react-vs-vanilla-js-example/react-vs-vanilla-js-example/react/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+
+function App() {
+  // Test 124
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+    </div>
+  );
+}
+
+export default App;
