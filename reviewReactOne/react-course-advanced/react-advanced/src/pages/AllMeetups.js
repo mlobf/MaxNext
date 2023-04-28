65e092ec54eeee2f93e3f6e6dc6ca0318c4be8f1
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-28 17:18:36 -0300

    Add a simple Routing + Switch

---
+++ reviewReactOne/react-course-advanced/react-advanced/src/pages/AllMeetups.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function AllMeetupsPage() {
+    return <div>AllMeetups Page</div>
+}
+
+export default AllMeetupsPage
---
+++ reviewReactOne/react-course-advanced/react-advanced/src/pages/NewMeetup.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function NewMeetupPage() {
+    return <div>NewMeetup Page</div>
+}
+
+export default NewMeetupPage
