e718035b9dd3a2cea1051788123205370b409b9f
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-29 13:17:17 -0300

    add basic form - New Meetup

--- reviewReactOne/react-course-advanced/react-advanced/src/pages/NewMeetup.js
+++ reviewReactOne/react-course-advanced/react-advanced/src/pages/NewMeetup.js
@@ -1,7 +1,12 @@
-import React from "react"
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
-    return <div>NewMeetup Page</div>
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm />
+        </section>
+    )
 }
 
 export default NewMeetupPage
