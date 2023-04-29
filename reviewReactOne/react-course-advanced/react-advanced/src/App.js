1005d8e5628668c1e5bda8e4a6419262969318ad
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-28 21:53:40 -0300

    Add Navigation Bat

--- reviewReactOne/react-course-advanced/react-advanced/src/App.js
+++ reviewReactOne/react-course-advanced/react-advanced/src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from "react-router-dom"
 import NewMeetupPage from "./pages/NewMeetup"
 import AllMeetupsPage from "./pages/AllMeetups"
 import FavoritesPage from "./pages/Favorites"
+import MainNavegation from "./components/layout/MainNavegation"
 
 function App() {
     return (
         <div>
+            <MainNavegation />
             <Switch>
                 <Route path="/" exact={true}>
                     <AllMeetupsPage />
